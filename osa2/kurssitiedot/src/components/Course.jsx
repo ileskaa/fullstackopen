305a3e47d46b803f88b56d7ac864ab002350a2eb
ileskaa <akseli.ingervo@gmail.com>
2025-01-04 08:14:13 +0200

    Complete exercise 2.5

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,43 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const initialVal = 0;
+  const sum = parts.reduce(
+    (accumulator, currentVal) => accumulator + currentVal.exercises,
+    initialVal
+  );
+
+  return <b>total of {sum} exercises</b>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
